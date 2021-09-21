e941b906315b59495f2d84cf70dfa9d19bd22759
bsc1154 <khl1154@naver.com>
2021-09-21 21:29:49 +0900

    Update and rename Solution.java to candidateKey.java

--- 18/candidateKey.java
+++ 18/candidateKey.java
@@ -1,40 +1,51 @@
 package test;
 
 import java.util.*;
+import java.util.logging.Logger;
 
-public class Solution {
+class candidateKey {
+    private final static Logger log = Logger.getGlobal();
+    static ArrayList<HashSet<Integer>> candidateKey;
+
     public static void main(String args[]){
-        String [][] a = {{"100","ryan","music","2"},{"200","apeach","math","2"},{"300","tube","computer","3"},{"400","con","computer","4"},{"500","muzi","music","3"},{"600","apeach","music","2"}};
-        System.out.println(solution(a));
+
+
+        String [][] relation = {{"100","ryan","music","2"},{"200","apeach","math","2"},{"300","tube","computer","3"},{"400","con","computer","4"},{"500","muzi","music","3"},{"600","apeach","music","2"}};
+
+        solution(relation);
 
     }
-    static ArrayList<HashSet<Integer>> candidateKey;
 
     public static int solution(String[][] relation){
         candidateKey = new ArrayList<>();
-        int colSize = relation[0].length;//1개원소의 원소개수 (열의 개수)
+        int colSize = relation[0].length;
 
         for(int i = 1 ; i <= colSize ; ++i) {
+            //후보키 추가
             makeKeySet(-1, colSize - 1, 0, i, new HashSet<>(), relation);
-            //-1, 열의개수, 0, 인덱스, keyset, 배열
         }
 
         return candidateKey.size();
     }
 
-    public static void makeKeySet(int attr, int maxAttr, int idx, int size, HashSet<Integer> keySet, String[][] relation) {
-        System.out.println("키셋"+keySet);
+
+   private static void makeKeySet(int attr, int maxAttr, int idx, int size, HashSet<Integer> keySet, String[][] relation) {
+        System.out.println("candidatekey!!"+candidateKey);
         if(idx == size) {
 
-            for(int i : keySet) System.out.print(i + " ");//
+            /*for(int i : keySet) System.out.print(i + " ");*/
 
             for(HashSet<Integer> key : candidateKey) {
+                System.out.println("keySet :"+keySet);
+                System.out.println("key :"+key);
                 if(keySet.containsAll(key)) {
                     System.out.println("는 " + key + "를 포함합니다.");
                     return;
                 }
             }
+            //최소성
 
+            //유일성
             if(isUnique(keySet, relation)) {
                 System.out.println("는 후보키 입니다.");
                 candidateKey.add(keySet);
@@ -44,6 +55,7 @@ public class Solution {
             return;
         }
 
+        //후보키 추가
         for(int i = attr + 1 ; i <= maxAttr ; ++i) {
             HashSet<Integer> newKeySet = new HashSet<>(keySet);
             newKeySet.add(i);
@@ -51,10 +63,9 @@ public class Solution {
         }
     }
 
-    //
     private static boolean isUnique(HashSet<Integer> keySet, String[][] relation) {
         HashMap<String, String> map = new HashMap<>();
-        //length 6(2차원 배열에서는 열)
+
         for(int r = 0 ; r < relation.length ; ++r) {
             String key = "";
 
@@ -62,8 +73,8 @@ public class Solution {
                 key += relation[r][c];
             }
 
-            if(map.containsKey(key)) return false;
-            else map.put(key, key);
+            if(map.containsKey(key)) return false;//유일한지 검사
+            else map.put(key, key);//유일하지 않으면 맵에 넣는다.
         }
         return true;
     }

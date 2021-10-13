46214921c083293d3faef17c1defe7c5779b1e98
bsc1154 <khl1154@naver.com>
2021-10-13 21:23:25 +0900

    Update matrixRotation.java
    feat : 행렬 테두리 회전하기 추가

--- 16/matrixRotation.java
+++ 16/matrixRotation.java
@@ -1,35 +1,13 @@
-import java.util.*;
+        int r1 = query[0]-1;// 테두리 범위 x
+        int c1 = query[1]-1;// 테두리 범위 y
+        int r2 = query[2]-1;// 테두리 범위 last x
+        int c2 = query[3]-1;// 테두리 범위 last y
 
-class Solution {
-    int[][] matrix;
-    public int[] solution(int rows, int columns, int[][] queries) {
-        this.matrix = new int[rows][columns];  // 행렬 생성
-        int[] answer = new int[queries.length]; // 정답 배열
-        
-        for(int i = 0; i < rows; i++){  // 행렬 초기화
-            for(int j = 0; j < columns; j++){
-                matrix[i][j] = i*columns + j + 1;
-            }
-        }
-        
-        for(int i = 0; i < queries.length; i++){ // 회전하고 최솟값 answer에 저장
-            answer[i] = rotate(queries[i]);
-        }
-        
-        return answer;
-    }
-    
-    public int rotate(int[] query){
-        int r1 = query[0]-1; 
-        int c1 = query[1]-1;
-        int r2 = query[2]-1;
-        int c2 = query[3]-1;
-        
         int temp = this.matrix[r1][c1]; // 시작위치 값 임시저장
         int min = temp;                 // min값 초기화
         for(int i = r1; i < r2; i++){ // 회전의 1번
-            this.matrix[i][c1] = this.matrix[i+1][c1];
-            if(min > this.matrix[i][c1]) min = this.matrix[i][c1];
+            this.matrix[i][c1] = this.matrix[i+1][c1]; //테두리를 한칸씩 이동하여 행렬 초기화
+            if(min > this.matrix[i][c1]) min = this.matrix[i][c1];//해당 테두리값중 최소값 구하기
         }
         for(int i = c1; i < c2; i++){ // 회전의 2번
             this.matrix[r2][i] = this.matrix[r2][i+1];
@@ -44,9 +22,6 @@ class Solution {
             if(min > this.matrix[r1][i]) min = this.matrix[r1][i];
         }
         this.matrix[r1][c1+1] = temp; // 임시저장한 값 저장
-        
+
         return min; 최솟값 반환
     }
-    
-}
- 
